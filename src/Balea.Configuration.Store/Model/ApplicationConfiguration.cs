86ba3f97bd460d9961892a699fabf6999a828d9b
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2020-03-19 15:03:45 +0100

    Initialize configuration

--- src/Balea.Configuration.Store/Model/ApplicationConfiguration.cs
+++ src/Balea.Configuration.Store/Model/ApplicationConfiguration.cs
@@ -3,7 +3,7 @@
     public class ApplicationConfiguration
     {
         public string Name { get; set; }
-        public RoleConfiguration[] Roles { get; set; } = new RoleConfiguration[0];
-        public DelegationConfiguration[] Delegations { get; set; } = new DelegationConfiguration[0];
+        public RoleConfiguration [] Roles { get; set; } = new RoleConfiguration[0];
+        public DelegationConfiguration [] Delegations { get; set; } = new DelegationConfiguration[0];
     }
 }
