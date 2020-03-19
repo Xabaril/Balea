946ed7e08368e9b728554fe2572f487f45d39177
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2020-03-19 12:07:17 +0100

    Remove unexpected token in source file

--- src/Balea.Configuration.Store/Model/ApplicationConfiguration.cs
+++ src/Balea.Configuration.Store/Model/ApplicationConfiguration.cs
@@ -5,5 +5,5 @@
         public string Name { get; set; }
         public RoleConfiguration[] Roles { get; set; } = new RoleConfiguration[0];
         public DelegationConfiguration[] Delegations { get; set; } = new DelegationConfiguration[0];
-;    }
+    }
 }
