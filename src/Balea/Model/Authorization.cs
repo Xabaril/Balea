956d9e02156ddda4fb124f57278043ff94f5337f
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2020-04-04 09:33:01 +0200

    Add authorization model for client & server

---
+++ src/Balea/Model/Authorization.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Balea.Model
+{
+    public class Authorization
+    {
+        public IEnumerable<Role> Roles { get; set; }
+        public IEnumerable<Delegation> Delegations { get; set; }
+    }
+}
