e91a85acc75856d63c7125a800026beae9499247
Hugo Biarge <hugo@arosbi.com>
2020-05-10 17:34:19 +0200

    Improve diagnostics experience (#16)

--- src/Balea/Diagnostics/EventIds.cs
+++ src/Balea/Diagnostics/EventIds.cs
@@ -4,8 +4,13 @@ namespace Balea.Diagnostics
 {
     internal static class EventIds
     {
-        public static readonly EventId BaleaMiddlewareThrow = new EventId(220, nameof(BaleaMiddlewareThrow));
-        public static readonly EventId BaleaMiddlewareAuthorizationSuccess = new EventId(221, nameof(BaleaMiddlewareAuthorizationSuccess));
-        public static readonly EventId BaleaMiddlewareActiveDelegation = new EventId(222, nameof(BaleaMiddlewareActiveDelegation));
+        public static readonly EventId AuthorizationPolicyFound = new EventId(220, nameof(AuthorizationPolicyFound));
+        public static readonly EventId AuthorizationPolicyNotFound = new EventId(221, nameof(AuthorizationPolicyNotFound));
+        public static readonly EventId CreatingAuthorizationPolicy = new EventId(222, nameof(CreatingAuthorizationPolicy));
+        public static readonly EventId NoBaleaRolesForUser = new EventId(230, nameof(NoBaleaRolesForUser));
+        public static readonly EventId ExecutingBaleaUnauthorizedFallback = new EventId(231, nameof(ExecutingBaleaUnauthorizedFallback));
+        public static readonly EventId NoBaleaRolesForUserAndNoUnauthorizedFallback = new EventId(232, nameof(NoBaleaRolesForUserAndNoUnauthorizedFallback));
+        public static readonly EventId PolicySucceed = new EventId(240, nameof(PolicySucceed));
+        public static readonly EventId PolicyFail = new EventId(241, nameof(PolicyFail));
     }
 }
