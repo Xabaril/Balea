cf7f774410291831731a652b9241b63b0b32e844
Luis Ruiz Pavón <luis_ruiz_pavon@hotmail.com>
2020-09-29 16:25:57 +0200

    Tags & Remove delegations restriction #30 #29

--- sample/WebAppEfCoreOidc/Infrastructure/Data/Migrations/20200929141638_Release_4_1_0.Designer.cs
+++ sample/WebAppEfCoreOidc/Infrastructure/Data/Migrations/20200929141638_Release_4_1_0.Designer.cs
@@ -4,14 +4,16 @@ using Balea.EntityFrameworkCore.Store.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace WebAppEfCoreOidc.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BaleaDbContext))]
-    partial class BaleaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200929141638_Release_4_1_0")]
+    partial class Release_4_1_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
