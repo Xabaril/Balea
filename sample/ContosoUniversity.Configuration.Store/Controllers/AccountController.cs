a1afc02c3caa6c667cff4ebcd368e67503f05baf
Miguel Esteban <miguel.esteban@educamos.com>
2020-11-12 12:45:31 +0100

    If model not valid, return view and display the error. jquery validate unobtrusive is broken and cause server exception.

--- sample/ContosoUniversity.Configuration.Store/Controllers/AccountController.cs
+++ sample/ContosoUniversity.Configuration.Store/Controllers/AccountController.cs
@@ -18,31 +18,36 @@ namespace ContosoUniversity.Configuration.Store.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginModel model)
         {
-            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
-
-            switch (model.UserName)
+            if (!ModelState.IsValid)
             {
-                case "john":
-                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "1"));
-                    identity.AddClaim(new Claim(ClaimTypes.Name, "john"));
-                    identity.AddClaim(new Claim(ClaimTypes.Role, "employee"));
-                    break;
-                case "mary":
-                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "2"));
-                    identity.AddClaim(new Claim(ClaimTypes.Name, "mary"));
-                    identity.AddClaim(new Claim(ClaimTypes.Role, "substitute"));
-                    break;
-                case "mark":
-                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "3"));
-                    identity.AddClaim(new Claim(ClaimTypes.Name, "mark"));
-                    identity.AddClaim(new Claim(ClaimTypes.Role, "employee"));
-                    break;
-                default:
-                    identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "4"));
-                    identity.AddClaim(new Claim(ClaimTypes.Name, model.UserName));
-                    identity.AddClaim(new Claim(ClaimTypes.Role, "customer"));
-                    break;
+                return View();
             }
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+           
+            switch (model.UserName)
+                {
+                    case "john":
+                        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "1"));
+                        identity.AddClaim(new Claim(ClaimTypes.Name, "john"));
+                        identity.AddClaim(new Claim(ClaimTypes.Role, "employee"));
+                        break;
+                    case "mary":
+                        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "2"));
+                        identity.AddClaim(new Claim(ClaimTypes.Name, "mary"));
+                        identity.AddClaim(new Claim(ClaimTypes.Role, "substitute"));
+                        break;
+                    case "mark":
+                        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "3"));
+                        identity.AddClaim(new Claim(ClaimTypes.Name, "mark"));
+                        identity.AddClaim(new Claim(ClaimTypes.Role, "employee"));
+                        break;
+                    default:
+                        identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, "4"));
+                        identity.AddClaim(new Claim(ClaimTypes.Name, model.UserName));
+                        identity.AddClaim(new Claim(ClaimTypes.Role, "customer"));
+                        break;
+                }
+ 
 
             await HttpContext.SignInAsync(
                 CookieAuthenticationDefaults.AuthenticationScheme,
